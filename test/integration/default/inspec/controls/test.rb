bf8d6538fda9974e99e92a84be7a35b99e0f558a
Paul Welch <pwelch@users.noreply.github.com>
2018-04-16 12:36:10 -0400

    Fix Skip Controls (#37)
    - Only include the local inspec test that depends on the CIS-Level1
    controls. If we use both, the skip controls are overwritten.
    - Add additional controls that are out of scope for this remediation
    cookbook
    - Switch back to kitchen output for Inspec results and comment out
    json results file for only when needed
    
    Fixes #36

--- test/integration/default/inspec/controls/test.rb
+++ test/integration/default/inspec/controls/test.rb
@@ -6,6 +6,14 @@ include_controls 'cis-centos7-level1' do
     xccdf_org.cisecurity.benchmarks_rule_1.1.7_Create_Separate_Partition_for_varlog
     xccdf_org.cisecurity.benchmarks_rule_1.1.8_Create_Separate_Partition_for_varlogaudit
     xccdf_org.cisecurity.benchmarks_rule_1.1.10_Add_nodev_Option_to_home
+    xccdf_org.cisecurity.benchmarks_rule_1.1.16_Add_noexec_Option_to_devshm_Partition
+
+    xccdf_org.cisecurity.benchmarks_rule_1.5.3_Set_Boot_Loader_Password
+
+    xccdf_org.cisecurity.benchmarks_rule_7.1.1_Set_Password_Expiration_Days
+
+    xccdf_org.cisecurity.benchmarks_rule_9.2.1_Ensure_Password_Fields_are_Not_Empty
+    xccdf_org.cisecurity.benchmarks_rule_9.2.12_Check_That_Users_Are_Assigned_Valid_Home_Directories
   )
 
   skipped_controls.each { |ctrl| skip_control ctrl }
