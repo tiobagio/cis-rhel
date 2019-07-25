9994958bd7abdc38eb8b526198a51c5743f01fab
John Snow <thelunaticscripter@outlook.com>
2019-07-25 08:04:56 -0400

    Add habitat plan so cookbook can be run as effortless (#125)
    Signed-off-by: John Snow <thelunaticscripter@outlook.com>

---
+++ habitat/plan.sh
@@ -0,0 +1,9 @@
+pkg_name=cis-rhel-chef
+pkg_origin=effortless-premium
+pkg_version="0.2.7"
+pkg_maintainer="Chef"
+pkg_license=("Proprietary - All Rights Reserved")
+pkg_scaffolding="chef/scaffolding-chef-infra"
+scaffold_policy_name="Policyfile"
+scaffold_policyfile_path="$PLAN_CONTEXT/../"
+scaffold_chef_dk="chef/chef-dk/3.0.38"
