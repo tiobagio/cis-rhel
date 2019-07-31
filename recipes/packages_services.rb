b14268092c52f3637edb9c50f06332936f01e367
Ross <rmoles@users.noreply.github.com>
2019-07-31 15:12:57 +0100

    Remove packages suggested by CIS (#130)
    * remove cis specified packages
    
    Signed-off-by: Ross Moles <rmoles@chef.io>
    
    * update unit tests
    
    Signed-off-by: Ross Moles <rmoles@chef.io>
    
    * update unit tests to use purge_package
    
    Signed-off-by: Ross Moles <rmoles@chef.io>
    
    * update unit tests to use rhel 7
    
    Signed-off-by: Ross Moles <rmoles@chef.io>
    
    * change purge to remove
    
    Signed-off-by: Ross Moles <rmoles@chef.io>

--- recipes/packages_services.rb
+++ recipes/packages_services.rb
@@ -3,18 +3,28 @@
 # Recipe:: packages_services
 #
 
-# 2.2.4_Ensure_CUPS_is_not_enabled
-service 'cups' do
-  action [:disable, :stop]
-end
-
 # 1.1.22_Disable_Automounting
 service 'autofs' do
   action [:disable, :stop]
 end
 
+# 1.2.5_Disable_the_rhnsd_Daemon
+service 'rhnsd' do
+  action [:disable, :stop]
+end
+
 # 1.5.4_Ensure_prelink_is_disabled
 package 'prelink' do
+  action :remove
+end
+
+# 1.6.1.4_Ensure_SETroubleshoot_is_not_installed
+package 'setroubleshoot' do
+  action :remove
+end
+
+# 1.6.1.5_Ensure_the_MCS_Translation_Service_mcstrans_is_not_installed
+package 'mcstrans' do
   action :remove
 end
 
@@ -24,7 +34,32 @@ yum_package 'xorg-x11*' do
   action      :remove
 end
 
-# 1.2.5_Disable_the_rhnsd_Daemon
-service 'rhnsd' do
+# 2.2.4_Ensure_CUPS_is_not_enabled
+service 'cups' do
   action [:disable, :stop]
+end
+
+# 2.3.1_Ensure_NIS_Client_is_not_installed
+package 'ypbind' do
+  action :remove
+end
+
+# 2.3.2_Ensure_rsh_client_is_not_installed
+package 'rsh' do
+  action :remove
+end
+
+# 2.3.3_Ensure_talk_client_is_not_installed
+package 'talk' do
+  action :remove
+end
+
+# 2.3.4_Ensure_telnet_client_is_not_installed
+package 'telnet' do
+  action :remove
+end
+
+# 2.3.5_Ensure_LDAP_client_is_not_installed
+package 'openldap-clients' do
+  action :remove
 end
--- spec/unit/recipes/packages_services_spec.rb
+++ spec/unit/recipes/packages_services_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'cis-rhel::packages_services' do
-  context 'By default on CentOS 7' do
+  context 'When all attributes are default, on RHEL 7' do
     cached(:chef_run) do
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner = ChefSpec::ServerRunner.new(platform: 'redhat', version: '7.6')
       runner.converge(described_recipe)
     end
 
@@ -28,6 +28,38 @@ describe 'cis-rhel::packages_services' do
     it 'disables and stops rhsnd service' do
       expect(chef_run).to disable_service 'rhnsd'
       expect(chef_run).to stop_service 'rhnsd'
+    end
+
+    it 'removes the setroubleshoot package' do
+      expect(chef_run).to remove_package 'setroubleshoot'
+    end
+
+    it 'removes the mcstrans package' do
+      expect(chef_run).to remove_package 'mcstrans'
+    end
+
+    it 'removes the ypbind package' do
+      expect(chef_run).to remove_package 'ypbind'
+    end
+
+    it 'removes the rsh package' do
+      expect(chef_run).to remove_package 'rsh'
+    end
+
+    it 'removes the talk package' do
+      expect(chef_run).to remove_package 'talk'
+    end
+
+    it 'removes the telnet package' do
+      expect(chef_run).to remove_package 'telnet'
+    end
+
+    it 'removes the openldap-clients package' do
+      expect(chef_run).to remove_package 'openldap-clients'
+    end
+
+    it 'removes the telnet package' do
+      expect(chef_run).to remove_package 'telnet'
     end
   end
 end
